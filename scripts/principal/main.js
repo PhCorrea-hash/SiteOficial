565631f5284becd50262755bec6e4711872e5234
Pedro Henrique <pedrohenriquemartinscorrea@gmail.com>
2025-02-28 01:19:08 -0300

    update

---
+++ scripts/principal/main.js
@@ -0,0 +1,9 @@
+const textoMain = `Profissional iniciante, com experiência em vendas, 
+pós-vendas e marketing digital, focado em atendimento ao cliente, 
+fidelização e suporte a estratégias digitais. Possui conhecimentos em ferramentas 
+como Microsoft Excel, MATLAB, HTML, CSS, JavaScript com ênfase em front-end e desenvolvimentos de web-sites. 
+Proativo, com boa capacidade de trabalho em equipe e motivação para aprender e crescer profissionalmente. 
+Em busca de uma oportunidade para contribuir e agregar valor à organização.`;
+
+const textoMainElemento = document.querySelector(".main-texto");
+textoMainElemento.textContent = textoMain;
